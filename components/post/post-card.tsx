b19c04325f0e15725969400523e49a035ef5a0bc
fultonoff <fultonkilebe7@gmail.com>
2023-10-29 23:25:18 +0100

    ✅updated post card

--- components/post/post-card.tsx
+++ components/post/post-card.tsx
@@ -2,6 +2,7 @@ import { Post } from '@/types/collection';
 import Image from 'next/image';
 import Link from 'next/link'
 import React from 'react'
+import PostContent from './post-content';
 
 
 interface PostProps{
@@ -9,11 +10,11 @@ interface PostProps{
 }
 const PostCard = ({post}: PostProps) => {
   return (
-    <Link href={`/post/${post.slug}`} className='grid grid-cols-2 gap-10'>
+    <Link href={`/post/${post.slug}`} className='grid grid-cols-2 gap-10 items-center'>
      {/* image */}
-     <Image src={post.image} width={600} height={300} alt={post.title}/>
+     <Image src={post.image} width={600} height={300} alt={post.title} className='rounded-md w-full object-cover object-center  max-h-[300px]'/>
      {/* Post content */}
-     <div>{post.title}</div>
+     <PostContent post={post}/>
     </Link>
   )
 }
---
+++ components/post/post-content.tsx
@@ -0,0 +1,32 @@
+import { Post } from '@/types/collection'
+import { ArrowBigDown, ArrowRight } from 'lucide-react'
+import React from 'react'
+
+
+interface PosrContentProps{
+    post: Post
+}
+const PostContent = ({post}: PosrContentProps) => {
+  return (
+    <div className='space-y-2'>
+        {/* Text */}
+        <div className='text-sm flex gap-2 items-center text-neutral-400'>
+            <div className={`font-medium ${post.category.title === 'Cities' ? 'text-emerald-500': 'text-indigo-600' }`}>{post.category.title}</div>
+            <div className='w-2 h-2 rounded-full bg-neutral-200'></div>
+            <div>{`${post.author.first_name} ${post.author.last_name}`}</div>
+            <div className='w-2 h-2 rounded-full bg-neutral-200'></div>
+            <div>1 min read</div>
+            <div className='w-2 h-2 rounded-full bg-neutral-200'></div>
+            <div>1  month ago</div>
+            <div className='w-2 h-2 rounded-full bg-neutral-200'></div>
+        </div>
+        {/* Title */}
+        <h2 className='font-medium text-3xl'>{post.title}</h2>
+        {/* Description */}
+        <p className='text-neutral-600 leading-snug'>{post.description}</p>
+        <div className='flex items-center pt-3'>Read More <ArrowRight size={14}/></div>
+    </div>
+  )
+}
+
+export default PostContent
